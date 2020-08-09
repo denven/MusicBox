6c975b81269e1185cb98cc0a5ea35e7e9ddeec5b
Chengwen <myregs@foxmail.com>
2020-08-09 14:56:58 -0700

    Completed leadboards page styling and music play features

--- src/views/leadboards/index.vue
+++ src/views/leadboards/index.vue
@@ -1,235 +1,354 @@
 <template>
-  <div id="newleadboards">
-    <SectionTitle :title="'Latest Leadboards'">
-      <i class="iconfont icon-paihangbang1"></i>
-    </SectionTitle>
-    <table class="el-table">
-      <thead>
-        <th v-for="item in leadboards" :key="item.id">{{ item.name }}</th>
-      </thead>
-      <tbody>
-        <tr v-for="(rowData, rowIdx) in tableData" :key="rowIdx">
-          <td v-for="(track, colIdx) in rowData" :key="colIdx">
-            <div class="playlist">
-              <span class="no">{{ rowIdx + 1 }}</span>
-              <div class="item-wrapper">
-                <span class="name">{{ track.name }}</span>
-                <div class="buttons">
-                  <i class="iconfont icon-bofang1" @click="playAudio(track)"></i>
-                  <i class="iconfont icon-add1"></i>
-                </div>
-              </div>
-            </div>
-          </td>
-        </tr>
-        <tr v-if="tableData.length > 0">
-          <td class="viewAll">
-            <el-button size="small">View Full List</el-button>
-          </td>
-          <td class="viewAll">
-            <el-button size="small">View Full List</el-button>
-          </td>
-          <td class="viewAll">
-            <el-button size="small">View Full List</el-button>
-          </td>
-          <td class="viewAll">
-            <el-button size="small">View Full List</el-button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+	<div class="leadboards" ref="wrapper">
+		<!-- Page Left Part contains three parts vertically -->
+		<div class="leadboard-wrapper">
+			<!-- Left Top Part: Playlist information -->
+			<div class="leadboard-detail">
+				<img class="leadboard-image" v-lazy="$helpers.getSmallPicture(curLb.coverImgUrl, 110)" alt="" />
+				<div class="leadboard-details">
+					<h3 class="name">{{ curLb.name }}</h3>
+					<div class="update-time">
+						<i class="iconfont icon-clock2"
+							>{{ `最后更新: ` + $helpers.formatTime(curLb.updateTime) }}
+							{{ $helpers.encloseNumber(curLb.updateFrequency) }}
+						</i>
+					</div>
+					<div class="description">{{ curLb.description }}</div>
+
+					<div class="social">
+						<span class="play">
+							<el-button size="mini" type="danger"> <i class="iconfont icon-bofang">Play</i> </el-button>
+							<el-button size="mini" type="danger"> <i class="iconfont icon-Add"></i> </el-button>
+						</span>
+						<el-button size="mini">
+							<i class="iconfont icon-shoucangjia2"> {{ $helpers.encloseNumber(curLb.subscribedCount) }}</i>
+						</el-button>
+						<el-button size="mini">
+							<i class="iconfont icon-ArtboardCopy"> {{ $helpers.encloseNumber(curLb.shareCount) }}</i>
+						</el-button>
+						<el-button size="mini">
+							<i class="iconfont icon-Clouddownload"> </i>
+						</el-button>
+						<el-button size="mini">
+							<i class="iconfont icon-message">{{ $helpers.encloseNumber(curLb.commentCount) }}</i>
+						</el-button>
+					</div>
+				</div>
+			</div>
+
+			<!-- Left Middle Part: Playlist(tracks) -->
+			<div class="song-list">
+				<div class="separator">
+					<div>
+						<span class="title">Tracks List</span><span class="song-count">{{ curLb.tracksCount + " tracks" }} </span>
+					</div>
+					<span class="play-count" v-if="curLb.playCount > 0">
+						Played: <span class="font-bold">{{ $helpers.formatNumberWithTS(curLb.playCount) }}</span>
+					</span>
+				</div>
+
+				<div class="songs-table">
+					<!-- <el-table :data="tracks.data" stripe style="width: 100%" :row-class-name="setRowIndex" @row-click="playAudio"> -->
+					<el-table stripe style="width: 100%" :data="lbTracks">
+						<el-table-column type="index" width="50"></el-table-column>
+						<el-table-column prop="name" label="Track">
+							<template class="name-wrapper" slot-scope="scope">
+								<img v-lazy="$helpers.getSmallPicture(scope.row.coverUrl, 50)" alt="" />
+								<span class="name" style="margin-left: 10px">{{ scope.row.name }}</span>
+								<div class="buttons">
+									<i class="iconfont icon-bofang1" @click="playAudio(scope.row)"></i>
+									<i class="iconfont icon-add1"></i>
+								</div>
+							</template>
+						</el-table-column>
+						<el-table-column prop="album" label="Album" width="220px"></el-table-column>
+						<el-table-column prop="artist" label="Artist" width="150px"></el-table-column>
+						<el-table-column prop="duration" label="Duration" width="80px"></el-table-column>
+					</el-table>
+				</div>
+			</div>
+
+			<!-- Left Bottom Part: Comments-->
+		</div>
+
+		<!-- Page Right Part -->
+		<div class="leadboard-list">
+			<div
+				v-for="(item, index) in leadboards"
+				class="leadboard-card"
+				@click="setSelected(index)"
+				:class="{ 'active-card': index == curSelected }"
+				:key="item.updateTime"
+			>
+				<img class="leadbaord-cover" v-lazy="$helpers.getSmallPicture(item.coverImgUrl, 50)" alt="" />
+				<div class="leadboard-desc">
+					<div class="name">{{ item.name }}</div>
+					<div class="update-freq">{{ item.updateFrequency }}</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-import SectionTitle from "@/components/pure-com/SectionTitle";
-import { getToplist, getAudioUrl, getAudioDetail } from "@/network/request";
+import { getAllLeadboards, getLeadboardDetail, getAudioUrl } from "@/network/request";
 
 export default {
-  data() {
-    return {
-      leadboards: [],
-      tableData: []
-    };
-  },
+	data() {
+		return {
+			leadboards: [],
+			curSelected: 0,
+			curLb: {},
+			lbTracks: [],
+		};
+	},
 
-  components: {
-    SectionTitle
-  },
+	methods: {
+		setSelected(index) {
+			this.curSelected = index;
+			this.curLb = this.leadboards[index];
+			this.$nextTick(() => {
+				this.$refs.wrapper.scrollIntoView({ behavior: "smooth" });
+			});
+		},
 
-  methods: {
-    async playAudio(track) {
-      try {
-        let res = await Promise.all([
-          Promise.resolve(getAudioUrl(track.id)),
-          Promise.resolve(getAudioDetail(track.id))
-        ]);
+		async getLeadboardDetail(id) {
+			let { data } = await getLeadboardDetail({ id });
+			let { shareCount, commentCount, playCount } = data.playlist;
 
-        const audioUrl = res[0].data.data[0].url;
-        const { picUrl } = res[1].data.songs[0].al; //album pic
-        const { name, artist } = track;
-        if (!audioUrl) {
-          return this.$message.error(
-            "This track is only available to VIP user！"
-          );
-        } else {
-          this.$store.state.song = {
-            name,
-            artist,
-            audioUrl,
-            picUrl
-          };
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    }
-  },
+			this.lbTracks = [];
+			data.playlist.tracks.forEach((item) => {
+				let { name, id, dt: duration } = item;
+				duration = this.$helpers.convertMsToMinutes(duration);
+				let album = item.al.name;
+				let artist = item.ar[0].name;
+				let coverUrl = item.al.picUrl;
+				this.lbTracks.push({ id, name, duration, album, artist, coverUrl });
+			});
 
-  // leadboards ids: 60198, 180106, 11641012, 120001
-  async created() {
-    let timestamp = Date.now();
-    let data = await Promise.all([
-      Promise.resolve(getToplist({ id: 60198, timestamp })),
-      Promise.resolve(getToplist({ id: 180106, timestamp })),
-      Promise.resolve(getToplist({ id: 11641012, timestamp })),
-      Promise.resolve(getToplist({ id: 120001, timestamp }))
-    ]);
+			this.curLb = { ...this.curLb, shareCount, commentCount, playCount, tracksCount: this.lbTracks.length };
+			console.log("====", this.lbTracks, data);
+		},
 
-    data = data.map(data => {
-      let {
-        id,
-        name,
-        desc,
-        coverImgUrl,
-        trackIds,
-        tracks,
-        playCount
-      } = data.data.playlist;
-      return { id, name, desc, coverImgUrl, trackIds, tracks, playCount };
-    });
+		async playAudio(track) {
+			try {
+				let { data } = await getAudioUrl(track.id);
+				const audioUrl = data.data[0].url;
 
-    this.leadboards = data;
+				if (!audioUrl) {
+					return this.$message.error("This track is only available to VIP user！");
+				} else {
+					this.$store.state.song = {
+						name: track.name,
+						artist: track.artist,
+						audioUrl,
+						picUrl: track.coverUrl,
+					};
+				}
+			} catch (error) {
+				console.log(error);
+			}
+		},
+	},
 
-    for (let i = 0; i < 20; i++) {
-      this.tableData.push([
-        {
-          name: data[0].tracks[i].name,
-          artist: data[0].tracks[i].al.name,
-          id: data[0].trackIds[i].id
-        },
-        {
-          name: data[1].tracks[i].name,
-          artist: data[1].tracks[i].al.name,
-          id: data[1].trackIds[i].id
-        },
-        {
-          name: data[2].tracks[i].name,
-          artist: data[2].tracks[i].al.name,
-          id: data[2].trackIds[i].id
-        },
-        {
-          name: data[3].tracks[i].name,
-          artist: data[3].tracks[i].al.name,
-          id: data[3].trackIds[i].id
-        }
-      ]);
-    }
+	watch: {
+		async curSelected() {
+			await this.getLeadboardDetail(this.curLb.id);
+		},
+	},
 
-    this.leadboards[0].name = "Billboard(Weekly)";
-    this.leadboards[1].name = "UK Charts(Weekly)";
-    this.leadboards[2].name = "iTunes(Weekly)";
-    this.leadboards[3].name = "Hit FM Top(Weekly)";
-  }
+	async created() {
+		let { data } = await getAllLeadboards();
+
+		data.list.forEach((list) => {
+			let {
+				updateFrequency,
+				trackUpdateTime,
+				updateTime,
+				trackCount,
+				commentThreadId,
+				coverImgUrl,
+				description,
+				trackNumberUpdateTime,
+				playCount,
+				subscribedCount,
+				name,
+				id,
+				ToplistType,
+			} = list;
+
+			this.leadboards.push({
+				updateFrequency,
+				trackUpdateTime,
+				updateTime,
+				trackCount,
+				commentThreadId,
+				coverImgUrl,
+				description,
+				trackNumberUpdateTime,
+				playCount,
+				subscribedCount,
+				name,
+				id,
+				ToplistType,
+			});
+		});
+
+		this.setSelected(15); // 15th is US billboard
+		await this.getLeadboardDetail(this.curLb.id);
+	},
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 @import "@/assets/styles/mixin.scss";
 @import "@/assets/styles/variables.scss";
 
-#newleadboards {
-  .playlist {
-    display: flex;
-    span.no {
-      align-self: center;
-      justify-self: end;
-      width: 20px;
-      margin-right: 10px;
-      text-align: right;
-      vertical-align: middle;
-    }
+.leadboards {
+	padding-top: 20px;
+	@include flex-align(row, flex-start, flex-start);
+}
 
-    .item-wrapper {
-      flex: 1;
-      @include flex-align(row, space-between, center);
-      // .buttons {
-      //   visibility: hidden;
-      // }
+// Left Part -- Top area styles
+.leadboard-wrapper {
+	flex: 1;
 
-      &:hover {
-        cursor: pointer;
-        .name {
-          text-decoration: underline;
-        }
-      }
+	.leadboard-detail {
+		width: 800px;
+		margin: 0 auto;
+		@include flex-align(row, flex-start);
+	}
 
-      .buttons {
-        i {
-          padding: 0 5px;
-          &:nth-child(1) {
-            color: $theme-primary-color;
-          }
-        }
-      }
-    }
-  }
+	.leadboard-image {
+		margin-right: 15px;
+	}
 
-  .el-table th {
-    text-align: center;
-    background-color: #f4f4f4;
-    color: black;
-  }
+	.leadboard-details {
+		height: 110px;
+		padding: 3px 0;
+		text-align: left;
+		@include flex-align(column, space-between, flex-start);
+		.name {
+			font-weight: bold;
+		}
 
-  .el-table td {
-    padding-left: 10px;
-    padding-right: 10px;
-    width: 25%;
-  }
+		.update-time i::before {
+			padding-left: 0 !important;
+		}
 
-  // the first 3 songs are in dark red
-  tbody tr:nth-child(-n + 3) .no {
-    color: rgb(196, 16, 16);
-  }
+		.update-time i,
+		.description {
+			font-size: 12px;
+		}
 
-  table {
-    margin-top: 20px;
-    margin-bottom: 20px;
-    border: 1px solid #0094ff;
-  }
+		.social > span > button {
+			padding: 7px 5px;
+		}
+		.social > span > button:nth-child(1) {
+			border-top-right-radius: 0;
+			border-bottom-right-radius: 0;
+		}
+		.social > span > button:nth-child(2) {
+			margin-left: 0.8px;
+			margin-right: 10px;
+			border-top-left-radius: 0;
+			border-bottom-left-radius: 0;
+		}
+	}
+}
 
-  table th,
-  table td {
-    border-left: 1px solid #0094ff;
-    border-right: 1px solid #0094ff;
-  }
+// Left Part -- Table area styles
+.song-list {
+	width: 100%;
+	margin-bottom: 20px;
 
-  table tr:last-child td {
-    border-bottom: 1px solid #0094ff;
-    text-align: center;
-  }
+	.separator {
+		height: 50px;
+		padding: 5px;
+		margin-top: 20px;
+		border-bottom: 2px solid $theme-primary-color;
+		@include flex-align(row, space-between);
+		div > .title {
+			font-size: 20px;
+			font-weight: bold;
+		}
+		div > .song-count {
+			font-size: 12px;
+			padding-left: 10px;
+		}
+		.play-count {
+			font-size: 12px;
+			span {
+				color: $theme-primary-color;
+				font-weight: bold;
+			}
+		}
+	}
 
-  table th {
-    font-weight: bold;
-    background-color: #f4f4f4;
-  }
+	/deep/tr td:nth-child(2) .cell {
+		@include flex-align(row, flex-start, center);
+		img {
+			width: 50px;
+		}
 
-  table tr:nth-child(even) {
-    background-color: #f4f4f4;
-  }
+		&:hover {
+			cursor: pointer;
+			.name {
+				text-decoration: underline;
+			}
+		}
 
-  table tr:nth-child(odd) {
-    background-color: #e8e8e8;
-  }
+		.name {
+			flex: 1;
+		}
+		.buttons {
+			width: 100px;
+			padding: 0 10px;
+			i {
+				padding: 0 5px;
+				&:nth-child(1) {
+					color: $theme-primary-color;
+				}
+			}
+		}
+	}
+}
+
+// Right Part styles
+.leadboard-list {
+	width: 280px;
+	padding-bottom: 20px;
+	margin-left: 20px;
+
+	.active-card {
+		background-color: rgba(0, 0, 0, 0.1);
+	}
+
+	.leadboard-card {
+		@include flex-align(row, flex-start);
+		margin-bottom: 10px;
+		cursor: pointer;
+		&:hover {
+			background-color: rgba(0, 0, 0, 0.05);
+		}
+
+		img {
+			padding-right: 10px;
+		}
+
+		.leadboard-desc {
+			padding: 5px 0;
+			height: 50px;
+			font-size: 14px;
+
+			@include flex-align(column, space-between, flex-start);
+			.name {
+				font-weight: 600;
+			}
+			.upate-freq {
+				color: lightgray;
+			}
+		}
+	}
 }
 </style>
