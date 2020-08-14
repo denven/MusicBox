7ce25e02b90b5f3bc57cc0b692f2e4e4b569a9b9
Chengwen <myregs@foxmail.com>
2020-08-14 12:44:17 -0700

    Update route push method parameters in ArtistCard: use named and path query way

--- src/components/pure-com/ArtistCard.vue
+++ src/components/pure-com/ArtistCard.vue
@@ -1,23 +1,29 @@
 <template>
-	<div class="artist-card" @click="viewArtist(artist)">
-		<img v-lazy="$helpers.getSmallPicture(artist.picUrl, 200)" alt="" />
-		<div class="artist-links">
-			<span class="artist-name">{{ artist.name }}</span>
-			<i class="iconfont icon-home_page" title="Personal Website"></i>
-		</div>
-	</div>
+  <div class="artist-card" @click="viewArtist(artist)">
+    <img v-lazy="$helpers.getSmallPicture(artist.picUrl, 200)" alt="" />
+    <div class="artist-links">
+      <span class="artist-name">{{ artist.name }}</span>
+      <i class="iconfont icon-home_page" title="Personal Website"></i>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-	name: "ArtistCard",
-	props: { artist: { type: Object } },
-	methods: {
-		viewArtist(artist) {
-			// this.$router.push(`/artists/detail` + `?id=${artistId}`);
-			this.$router.push({ name: "ar-detail", params: { artist } });
-		},
-	},
+  name: "ArtistCard",
+  props: { artist: { type: Object } },
+  methods: {
+    viewArtist(artist) {
+      // this.$router.push(`/artists/detail` + `?id=${artistId}`);
+      // this.$router.push({ name: "ar-detail", params: { artist } });
+      this.$router.push({
+        name: "ar-detail",
+        path: "/artists/detail",
+        query: { id: artist.id },
+        params: { artist }
+      });
+    }
+  }
 };
 </script>
 
@@ -26,22 +32,22 @@ export default {
 @import "@/assets/styles/variables.scss";
 
 .artist-card {
-	width: 200px;
-	margin-bottom: 20px;
+  width: 200px;
+  margin-bottom: 20px;
 
-	img {
-		width: 100%;
-		border-radius: 5px;
-		cursor: pointer;
-	}
+  img {
+    width: 100%;
+    border-radius: 5px;
+    cursor: pointer;
+  }
 
-	.artist-links {
-		padding: 5px;
-		font-size: 12px;
-		@include flex-align(row, space-between);
-		i {
-			color: $theme-primary-color;
-		}
-	}
+  .artist-links {
+    padding: 5px;
+    font-size: 12px;
+    @include flex-align(row, space-between);
+    i {
+      color: $theme-primary-color;
+    }
+  }
 }
 </style>
